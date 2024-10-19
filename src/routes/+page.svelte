d90fcc6bb0aa1845b1a25339d0a13c1e51b58502
Tyson Fairhurst <55992872+fairhurt@users.noreply.github.com>
2024-10-19 15:45:25 -0700

    Update example

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,20 @@
 <script lang="ts">
-  import {
-    createVirtualizedTree,
-    type TreeItem,
-    VirtualizedTree,
-  } from "svelte-virtualized-tree";
+  import type { VirtualTreeItem } from "svelte-virtualized-tree";
+  import { createTreeVirtualizer } from "svelte-virtualized-tree";
+  import Plus from "lucide-svelte/icons/plus";
+  import ChevronRight from "lucide-svelte/icons/chevron-right";
+  import ChevronDown from "lucide-svelte/icons/chevron-down";
+  import Check from "lucide-svelte/icons/check";
+  import Minus from "lucide-svelte/icons/minus";
+  import { createVirtualizer } from "@tanstack/svelte-virtual";
+
+  const icons = {
+    plus: Plus,
+    "chevron-right": ChevronRight,
+    "chevron-down": ChevronDown,
+    check: Check,
+    minus: Minus,
+  };
   type CustomTreeItem = {
     displayName: string;
   };
@@ -12,12 +23,12 @@
     level: number,
     parentId: number | null,
     count: number
-  ): TreeItem<CustomTreeItem>[] {
-    const items: TreeItem<CustomTreeItem>[] = [];
+  ): VirtualTreeItem<CustomTreeItem>[] {
+    const items: VirtualTreeItem<CustomTreeItem>[] = [];
 
     for (let i = 0; i < count; i++) {
       const id = Math.floor(Math.random() * 10000);
-      const item: TreeItem<CustomTreeItem> = {
+      const item: VirtualTreeItem<CustomTreeItem> = {
         id,
         displayName: `Node ${id}`,
         icon: "plus",
@@ -25,7 +36,7 @@
         parentId,
         children:
           level < 4
-            ? generateTreeData(level + 1, id, Math.floor(Math.random() * 3))
+            ? generateTreeData(level + 1, id, Math.floor(Math.random() * 4))
             : [],
       };
       totalNodes++;
@@ -33,23 +44,92 @@
     }
     return items;
   }
-  const data = generateTreeData(0, null, 1000);
-  const virtualizedTree = createVirtualizedTree({
-    data,
+
+  const treeData: VirtualTreeItem<CustomTreeItem>[] = generateTreeData(
+    0,
+    null,
+    2000
+  );
+
+  let virtualListEl: HTMLDivElement;
+  let virtualTreeEl: HTMLDivElement;
+
+  $: virtualizedTree = createTreeVirtualizer<
+    HTMLDivElement,
+    HTMLDivElement,
+    CustomTreeItem
+  >({
+    getScrollElement: () => virtualTreeEl,
+    data: treeData,
     accessorKey: "displayName",
   });
+
+  $: virtualizer = createVirtualizer({
+    count: 0,
+    estimateSize: () => 35,
+    getScrollElement: () => virtualListEl,
+    overscan: 5,
+  });
+  $: {
+    // Prevents re-creating the virtualizer if the size changes.
+    $virtualizer.setOptions({
+      count: $virtualizedTree.getVirtualTreeItems().length,
+    });
+  }
 </script>
 
-<div>
-  <h1>Virtualized Tree</h1>
-  <p>
-    This is a virtualized tree component that renders a large number of nodes
-    efficiently.
-  </p>
-  <p>
-    Total nodes: {totalNodes}
-  </p>
-  <div style="height: 400px; overflow-y: auto;">
-    <VirtualizedTree {virtualizedTree} />
+\
+<h4 class=" scroll-m-20 text-xl font-semibold tracking-tight">
+  Basic Tree with {totalNodes} total elements.
+</h4>
+<div style="height: 600px; overflow:auto;" bind:this={virtualTreeEl}>
+  <div style="" bind:this={virtualListEl}>
+    <div
+      style="position: relative; height: {$virtualizer.getTotalSize()}px; width: 100%;"
+    >
+      {#each $virtualizer.getVirtualItems() as row (row.index)}
+        {@const node = $virtualizedTree.visibleItems[row.index]}
+        {@const hasChildren = !!node.children?.length}
+        {@const id = node.id}
+        {@const icon = node.icon}
+        {@const isExpanded = $virtualizedTree.expandedNodes.has(id)}
+        {@const content = node[$virtualizedTree.options.accessorKey]}
+        {@const selectedId = $virtualizedTree.getSelectedId()}
+
+        <div
+          class:list-item-even={row.index % 2 === 0}
+          class:list-item-odd={row.index % 2 === 1}
+          style="position: absolute; top: 0; left: 0; width: 100%; height: {row.size}px; transform: translateY({row.start}px); padding-left: {$virtualizedTree
+            .visibleItems[row.index].level * 20}px;"
+        >
+          <button
+            style="display: flex; align-items: center; gap: 0.25rem; white-space: nowrap; padding: 0.25rem; background-color: {$virtualizedTree.getSelectedId() ===
+            id
+              ? 'rgb(203 213 225);'
+              : ''};"
+            on:click={() => {
+              $virtualizedTree.toggleNode(id, row.index);
+            }}
+            aria-expanded={isExpanded}
+          >
+            <!-- Add icon. -->
+            {#if icon === "plus" && hasChildren && isExpanded}
+              <svelte:component this={icons["chevron-down"]} class="h-4 w-4" />
+            {:else if icon === "plus" && hasChildren && !isExpanded}
+              <svelte:component this={icons["plus"]} class="h-4 w-4 " />
+            {/if}
+            <span class="select-none">{content}</span>
+            <!-- Selected icon. -->
+            {#if selectedId === id}
+              <svelte:component
+                this={icons["check"]}
+                class="h-4 w-4 "
+                style="stroke:#22c55e"
+              />
+            {/if}
+          </button>
+        </div>
+      {/each}
+    </div>
   </div>
 </div>
